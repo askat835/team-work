1c9a7180efb43b94112778ec8ffef78df4b9e983
Your Name <you@example.com>
2025-06-14 22:58:43 +0600

    add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+// import React, { useState } from 'react';
+
 
 function App() {
-  const [count, setCount] = useState("")
-  const text = () =>{
-   return setCount('Acer Nitro 5')
-  }
+
 
   return (
-    // <>
-    //   <div>
-    //     <a href="https://vite.dev" target="_blank">
-    //       <img src={viteLogo} className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://react.dev" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </>
-   <div>
-      <h1>Satarov Askatbek</h1>
-      <button onClick={text}>Click me</button>
-      <h2 style={{backgroundColor : 'green'}}>{count}</h2>
-
+    <div>
+   
       
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+
+
+export function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: 50 }}>
+      <h1>Кош келиңиз, Аскат!</h1>
+      <p>Бул сиздин негизги бет (Home Page)</p>
+    </div>
+  );
+}
